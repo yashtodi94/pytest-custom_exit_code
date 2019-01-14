40bbc4901fcb3eeac4f7cc050d16e14e92a38b8f
Yash Todi <yash.todi@beautifulcode.in>
2019-01-14 15:43:38 +0530

    master: Updated version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-custom_exit_code',
-    version='0.1.0',
+    version='0.1.1',
     author='Yash Todi',
     author_email='yashtodi94@gmail.com',
     maintainer='Yash Todi',
